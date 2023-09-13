fdf292cd4c23b0088ce52e07ed0bbfb64d20d2dc
Michael <Michael@SKorik.com>
2023-09-13 22:48:00 +0300

    Ex_10

---
+++ Users/micha/OneDrive/Рабочий стол/python/homework2.py
@@ -0,0 +1,6 @@
+orel = int(input())
+reshka = int(input())
+if orel > reshka or orel == reshka:
+    print(f"минимальное количество переворотов - {reshka}")
+else:
+    print(f"минимальное количество переворотов - {orel}")
